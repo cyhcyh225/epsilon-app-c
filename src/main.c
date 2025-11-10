211fdfeb120e75f35e69b9ee031a01044f35a63d
cyhcyh225 <123456cyhcyh@gmail.com>
2025-11-10 16:50:43 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,7 +3,8 @@
 #include <string.h>
 #include <stdio.h>
 
-
+#define LCD_REG (*((volatile unsigned short *) 0x60000000))
+#define LCD_DATA (*((volatile unsigned short *) (0x60000000 | (1<<(16+1))) ))
 
 const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "App";
 const uint32_t eadk_api_level  __attribute__((section(".rodata.eadk_api_level"))) = 0;
@@ -17,8 +18,7 @@ void move_pointer() {
   }
 }
 /*
-#define LCD_REG (*((volatile unsigned short *) 0x60000000))
-#define LCD_DATA (*((volatile unsigned short *) (0x60000000 | (1<<(16+1))) ))
+
 static inline void send_command(uint8_t c) {
     LCD_REG = c;
 }
@@ -50,6 +50,8 @@ int main(int argc, char * argv[]) {
 }
 */
 int main(int argc, char * argv[]) {
-  eadk_display_draw_string("hello", (eadk_point_t){0, 0}, true, eadk_color_black, eadk_color_white);
+  char msg[30];
+  sprintf(msg, "id=0x%02X%02X%02X\r\n", 2, 3, 4);
+  eadk_display_draw_string(msg, (eadk_point_t){0, 0}, true, eadk_color_black, eadk_color_white);
   move_pointer();
 }
